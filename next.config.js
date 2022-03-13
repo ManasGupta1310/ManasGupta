d8245878946a2511cb8d40cf5dbfb3d2845ffc68
Manas Gupta <mmanasggupta@gmail.com>
2022-03-13 14:50:57 +0530

    Changes

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/ManasGupta1310/manasgupta1310.github.io/gh-pages/' : '',
-}
