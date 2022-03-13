0e730d9d4dd1c7e106a8921da306a37b680ac9b6
Manas Gupta <mmanasggupta@gmail.com>
2022-03-13 21:01:44 +0530

    FIx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import '../styles/globals.css';
-import "../.next/static/sample.css"
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
