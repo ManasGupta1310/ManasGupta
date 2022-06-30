80068564c5b7306b7a33b95c1edfe70fb947eb49
Manas Gupta <mmanasggupta@gmail.com>
2022-06-30 15:14:10 +0530

    Add content and fix stuff

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,24 +11,22 @@ export const About = (): JSX.Element => {
     >
       <h1>About</h1>
       <div className={styles.aboutPara}>
-        <div className={styles.aboutFirst}>
-          <p style={{ fontSize: 18 }}>Hi my name is Manas Gupta.
-            I am a second year UG student at Indian Institute of Technology, Kanpur,
-            doing by Bachlelors in Computer Science and Engineering. 
-            <br/>
-             <br/>
-            I live in Kanpur. I have shifted a lot over my life so I did my schooling in cities like Kanpur,
-             Ghaziabad and Delhi and lived in Nepal and Kota as well. Living in Kota would obviously mean preparing for JEE.
-             <br/>
-             <br/>
-            Having managed to get through my first year, I had learned various things
-            during that time.
-            <br/>
-            <br/>
-            I am interested in WebDev and ML and I frequently involve myself in Competitive Programming. Delving deep to solve 
-            some lame or some fascinating problem si really the sort of thing I am looking for.
-          </p>
-        </div>
+        <p style={{ fontSize: 18 }}>Hi my name is Manas Gupta.
+          I am a second year UG student at Indian Institute of Technology, Kanpur,
+          doing by Bachlelors in Computer Science and Engineering.
+          <br />
+          <br />
+          I live in Kanpur. I have shifted a lot over my life so I did my schooling in cities like Kanpur,
+          Ghaziabad and Delhi and lived in Nepal and Kota as well. Living in Kota would obviously mean preparing for JEE.
+          <br />
+          <br />
+          Having managed to get through my first year, I had learned various things
+          during that time.
+          <br />
+          <br />
+          I am interested in WebDev and ML and I frequently involve myself in Competitive Programming. Delving deep to solve
+          some lame or some fascinating problem si really the sort of thing I am looking for.
+        </p>
       </div>
     </Layout>
   );
